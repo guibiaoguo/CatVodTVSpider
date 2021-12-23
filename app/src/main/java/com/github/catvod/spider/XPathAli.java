d3c940e5de24a37195ba28dbccb8f4b54cb15523
guibiaoguo <guoa1234@163.com>
2021-12-23 19:58:05 +0800

    update 阿里云盘的xpath

--- app/src/main/java/com/github/catvod/spider/XPathAli.java
+++ app/src/main/java/com/github/catvod/spider/XPathAli.java
@@ -148,10 +148,9 @@ public class XPathAli extends XPath {
                 JSONObject headerObj = new JSONObject();
                 headerObj.put("User-Agent","Mozilla/5.0 (Linux; Android 11; Mi 10 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36");
                 JSONObject result = new JSONObject();
-                result.put("parse", 1);
-                result.put("playUrl", "");
+                result.put("parse", 0);
+                result.put("playUrl", "https://okjx.cc/?url=");
                 result.put("url", videoUrl);
-                result.put("jx","1");
                 result.put("header", headerObj.toString());
                 return result.toString(4);
             }
